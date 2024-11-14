6884b2e3b14e264706ffa7bfe0efcf9e79497905
wesllanSilva <wesllan90@gmail.com>
2024-11-14 11:05:37 -0300

    feat: add desafios docker. desafio 1 volumes

---
+++ Docker/gerenciando_volumes.py
@@ -0,0 +1,19 @@
+input_string = input()
+
+def docker_command(input_string):
+    # Separa a entrada em ação e nome do volume
+    action, volume_name = input_string.split(", ")
+    
+    # COMPLETE AQUI: Mapeie as ações para comandos Docker correspondentes
+    actions_to_commands = {
+        "criar": "docker volume create",
+        "inspecionar": "docker inspect",
+        "remover": "docker volume rm "
+    }
+    
+    # Verifica se a ação está mapeada e retorna o comando correspondente
+    if action in actions_to_commands:
+        return f"{actions_to_commands[action]} {volume_name}"
+
+# Gera e exibe o comando Docker correspondente
+print(docker_command(input_string))
